643c2a731ded6c3b2acff3af7c061ebff326594e
vipulshete <vipulshete724@gmail.com>
2022-08-01 18:24:10 +0530

    linklist

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.bridgelab.linklist;
+
+import com.bridege.linklist.Nodes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Nodes<Integer> firstNode = new Nodes<>(56);
+		
+		Nodes<Integer> secondNode = new Nodes<>(30);
+		
+		Nodes<Integer> thirdNode = new Nodes<>(70);
+
+	}
+
+}
---
+++ Nodes.java
@@ -0,0 +1,31 @@
+package com.bridgelab.linklist;
+
+
+
+	public class Nodes<D> {
+
+		private D data;
+	
+	private Nodes<D> nextNode;
+	
+	public Nodes(D data) {
+		this.data = data;
+		System.out.println("NODE-" + data);
+	}
+	
+	public void setNextNode(Nodes<D> nextNode) {
+		this.nextNode = nextNode;
+	}
+
+	public D getData() {
+		return data;
+	}
+
+	public void setData(D data) {
+		this.data = data;
+	}
+
+	public Nodes<D> getNextNode() {
+		return nextNode;
+	}
+}
